4050a7843d7aa6f2baf6707848532c63b745b87b
HassanIqbal715 <158496042+HassanIqbal715@users.noreply.github.com>
2026-02-01 22:01:59 +0500

    Specify open modes for the file

--- assignment01/task02/file.h
+++ assignment01/task02/file.h
@@ -3,16 +3,20 @@
 #include<string>
 using namespace std;
 
+enum FileMode {READ, WRITE, READ_WRITE};
+
 class File {
 private:
     string path;
     fstream file;
+    FileMode openMode;
 
 public:
     File(string path);
+    File(string path, FileMode openMode);
     void createFile();
     bool openFile();
-    void writeFile(string line);
+    void writeFile(string line, bool pointerReset);
     string* readFile();
     ~File();
 };
