1da0ba448e832352f866ff9c9c54c44dee41b897
HassanIqbal715 <158496042+HassanIqbal715@users.noreply.github.com>
2026-02-01 22:04:03 +0500

    Write function definitions.
    - Definition added for writeFile, openFile and readFile functions.
    - Constructors and deconstructors defined.
    - Code adjusted for the new openMode.
    
    Signed-off-by: HassanIqbal715 <158496042+HassanIqbal715@users.noreply.github.com>

--- assignment01/task02/file.cpp
+++ assignment01/task02/file.cpp
@@ -1,8 +1,15 @@
 #include"file.h"
+#include<iostream>
 #include<filesystem>
 
 File::File(string path) {
     this->path = path;
+    this->openMode = FileMode::READ_WRITE;
+}
+
+File::File(string path, FileMode openMode) {
+    this->path = path;
+    this->openMode = openMode;
 }
 
 void File::createFile() {
@@ -28,4 +35,58 @@ void File::createFile() {
 
     ofstream f(filePath);
     if (f.is_open()) f.close();
+}
+
+bool File::openFile() {
+    if (file.is_open()) {
+        return true;
+    }
+
+    std::filesystem::path filePath = path;
+
+    if (!std::filesystem::exists(filePath))
+        createFile();
+
+    if (openMode == FileMode::READ_WRITE)
+        file.open(filePath, ios::in | ios::out);
+    else if (openMode == FileMode::READ)
+        file.open(filePath, ios::in);
+    else if (openMode == FileMode::WRITE)
+        file.open(filePath, ios::out);
+    else 
+        file.open(filePath, ios::in | ios::out);
+
+    return file.is_open();
+}
+
+void File::writeFile(string line, bool pointerReset = false) {
+    if (!openFile() || openMode == FileMode::READ) return;
+
+    file.clear(); // reset the flags
+    if (pointerReset) {
+        file.seekg(0, ios_base::beg); // start writing from the 1st character
+    }
+    
+    file << line << endl;
+}
+
+string* File::readFile() {
+    if (!openFile() || openMode == FileMode::WRITE) return nullptr;
+
+    file.seekp(0, ios_base::beg);
+
+    string line;
+    string* data = new string[500];
+    int index = 0;
+    while(getline(file, line)) {
+        data[index++] = line;
+    }
+
+    data[index] = '\0';
+    return data;
+}
+
+File::~File() {
+    if (file.is_open())
+        file.close();
 }
