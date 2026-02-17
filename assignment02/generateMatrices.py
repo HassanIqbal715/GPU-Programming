c6564bade19ad62c05d2f4c4d07c6b2ac08c5067
HassanIqbal715 <158496042+HassanIqbal715@users.noreply.github.com>
2026-02-18 00:03:17 +0500

    Adjust element value to produce only 2's

--- assignment02/generateMatrices.py
+++ assignment02/generateMatrices.py
@@ -9,7 +9,12 @@ cols = [100, 200]
 
 script_dir = Path(__file__).parent
 
-# Takes max 3 arguments n rows cols.
+# file takes either 2 or 4 arguments
+# First 2 arguments are for the rows and columns for the first matrix
+# Second 2 arguments are for the rows and columns for the second matrix
+# Defaults are used if none provided
+# First matrix updated if only 2 provided
+
 if len(sys.argv) > 1:
     if len(sys.argv) == 3:
         rows[0] = int(sys.argv[1])
@@ -25,10 +30,10 @@ file_path = script_dir / "data" / "input.txt"
 file_path.parent.mkdir(parents=True, exist_ok=True)
 
 file = open(file_path, 'w')
-file.write(str(n) + '\n')
+# file.write(str(n) + '\n')
 
 for i in range(0, n):
     file.write(str(rows[i]) + " " + str(cols[i]) + '\n')
-    row_data = [str(random.randint(0, 100)) for _ in range(cols[i] * rows[i])]
-    file.write(" ".join(row_data) + " ")
+    row_data = [str(2) for _ in range(cols[i] * rows[i])]
+    file.write(" ".join(row_data))
     file.write("\n")
