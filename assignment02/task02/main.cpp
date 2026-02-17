caaf445e622c4f5ea308c9d06c1799a0f0a94e1b
HassanIqbal715 <158496042+HassanIqbal715@users.noreply.github.com>
2026-02-18 00:08:19 +0500

    Complete task02

--- assignment02/task02/main.cpp
+++ assignment02/task02/main.cpp
@@ -15,57 +15,32 @@ int main(int argc, char *argv[]) {
     string inputPath = "../../data/input.txt";
     string outputPath = "../../data/output.txt";
 
+    cout << "========================\n";
     handleArguments(inputPath, outputPath, argc, argv);
+    cout << "========================\n";
 
     // Read and extract data from inputFile
-    File *inputFile = new File(inputPath, FileMode::READ);
+    vector<Matrix> matrices = loadData(inputPath);
 
-    vector<string> inputDataString = inputFile->readFile();
-
-    if (inputDataString.size() == 0) {
-        cerr << "Error! Empty input file provided!\n";
+    if (matrices.size() < 2) {
+        cerr << "Error: File not compatible or empty\n";
         return 1;
     }
 
-    int *numberOfMatrices = extractInputData(inputDataString[0], 1, 1);
-    int n = numberOfMatrices[0];
-
-    vector<int *> inputDataMatrices;
-
-    int *sizeArray;
-    int resultRows = 0, resultColumns = 0, intermediateRows = 0;
-
-    int *tempArray;
-
-    for (int i = 1; i < (n * 2) + 1; i++) {    
-        sizeArray = extractInputData(inputDataString[i++], 1, 2);
-        // Reading the first matrix's size
-        if (i - 1 == 1) {
-            resultRows = sizeArray[0];
-            intermediateRows = sizeArray[1];
-        }
-        // Reading the last matrix's size
-        if (i == n * 2)
-            resultColumns = sizeArray[1];
-
-        tempArray = extractInputData(inputDataString[i], sizeArray[0], sizeArray[1]);
-        inputDataMatrices.push_back(tempArray);
-        delete[] sizeArray;
-    }
-
-    // Immediately clear the read data because it is of no use anymore.
-    delete inputFile;
-
-    File *outputFile = new File(outputPath, FileMode::WRITE);
-
     // Result matrix
-    double *C = createMatrix(resultRows, resultColumns);
+    Matrix result;
+    createMatrix(result, matrices[0].rows, matrices[1].cols);
 
     auto startTimePoint = chrono::steady_clock::now();
     
     // Compute the sum and store it in result matrix
-    cpuMul(C, inputDataMatrices[0], inputDataMatrices[1], resultRows, 
-        intermediateRows, resultColumns);
+    try {
+        cpuMul(result, matrices[0], matrices[1]);
+    }
+    catch(runtime_error err) {
+        cerr << "Error: " << err.what() << "\n";
+        return 1;
+    }
 
     auto endTimePoint = chrono::steady_clock::now();
 
@@ -78,14 +53,6 @@ int main(int argc, char *argv[]) {
     cout << "seconds" << endl;
 
     // Write the result to the output file
-    writeMatrix(C, n, resultRows, resultColumns, outputFile);
-    cout << "Successfully written output to: " << outputPath << endl;
-
-    // Clear allocated memory
-    for (int i = 0; i < n; i++) {
-        delete[] inputDataMatrices[i];
-    }
-
-    delete[] C;
-    delete outputFile;
+    writeMatrix(result, outputPath);
+    cout << "\nSuccessfully written output to: " << outputPath << endl;
 }
